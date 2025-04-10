ec5225600b9c4b3fa991e281224ce3bb002f0ebd
Hayden Barogh <hbarogh@gmail.com>
2025-04-10 14:22:24 -0500

    Changed the cloudinary folder to hopefully  only be the Vercel-Test folder.

--- pages/index.tsx
+++ pages/index.tsx
@@ -139,7 +139,7 @@ export default Home;
 
 export async function getStaticProps() {
   const results = await cloudinary.v2.search
-    .expression("")
+    .expression(`folder:"${process.env.CLOUDINARY_FOLDER}"`)
     .sort_by("public_id", "desc")
     .max_results(400)
     .execute();
