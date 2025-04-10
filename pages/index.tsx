ccfaee9cbdc99eec0491b301187f6a02aab6c792
Hayden Barogh <hbarogh@gmail.com>
2025-04-10 14:35:32 -0500

    Changed the text for the paragraph on the page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,8 +62,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               My Photography Portfolio
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              Our incredible Next.js community got together in San Francisco for
-              our first ever in-person conference!
+              Enjoy looking at some of my photos from my photography portfolio!
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
